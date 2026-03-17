bf89414a3dbef38dc81fd6b0a4c6f8bb4795e1fa
mpinedaa4 <mpinedaa4@eafit.edu.co>
2026-03-17 15:02:05 -0500

    Tutorial #6

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { HomeModule } from './home/home.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    HomeModule,
+    BooksModule,
+  ],
+})
+export class AppModule {}
+
