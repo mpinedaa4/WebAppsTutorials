018747b663422b657d18370ed330e315e111c4e0
mpinedaa4 <mpinedaa4@eafit.edu.co>
2026-04-09 19:11:08 -0500

    Tutorial #8

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
